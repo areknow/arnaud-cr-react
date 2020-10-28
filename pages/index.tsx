d2550b780254cc11a6ba733ca0f06b18fe857345
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-10-28 19:59:35 -0400

    typescript

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>x</title>
+      </Head>
+      <main>x</main>
+    </>
+  );
+};
+export default Home;
